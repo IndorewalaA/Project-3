b4e69110c6be5dfa20858d136b7ddf7beb32f8c2
IndorewalaA <abdul.indorewala@ufl.edu>
2024-04-16 20:06:07 -0400

    pushes back all songs to the vector.

--- main.cpp
+++ main.cpp
@@ -10,23 +10,70 @@ using namespace std;
 struct Song
 {
     string isrc;
-    double acousticness;
-    double danceability;
-    double duration;
-    double energy;
-    double instrumentality;
-    double key; //NOT USED
-    double livelihood;
-    double loudness;
-    double mode; // NOT USED
-    double speechiness; // DONT USE IF YOU DONT WANT TO
-    double tempo;
-    double valence; // THIS IS SO COOLLLL
+    double acousticness = 0;
+    double danceability = 0;
+    int duration = 0;
+    double energy = 0;
+    double instrumentality = 0;
+    double key = 0; //NOT USED
+    double livelihood = 0;
+    double loudness = 1;
+    int mode = 0; // NOT USED
+    double speechiness = 0; // DONT USE IF YOU DONT WANT TO
+    double tempo = 0;
+    string time_signature;
+    double valence = 0; // THIS IS SO COOLLLL
 };
 
 int main() {
     vector<Song> songList;
     ifstream input("updated_audio_features.csv");
+    if (!input.is_open()) {
+        cout << "File not open! Exiting program..." << endl;
+        return 0;
+    }
+    // isrc,acousticness,danceability,duration_ms,energy,instrumentalness,key,liveness,loudness,mode,speechiness,
+    // tempo,time_signature,valence,updated_on
 
+    string header;
+    getline(input, header);
+
+    string line;
+    while(getline(input, line)) {
+        string temp;
+        stringstream stream(line);
+        Song song;
+
+        getline(stream, song.isrc, ',');
+        getline(stream, temp, ',');
+        song.acousticness = stod(temp);
+        getline(stream, temp, ',');
+        song.danceability = stod(temp);
+        getline(stream, temp, ',');
+        song.duration = stoi(temp);
+        getline(stream, temp, ',');
+        song.energy = stod(temp);
+        getline(stream, temp, ',');
+        song.instrumentality = stod(temp);
+        getline(stream, temp, ',');
+        song.key = stod(temp);
+        getline(stream, temp, ',');
+        song.livelihood = stod(temp);
+        getline(stream, temp, ',');
+        song.loudness = stod(temp);
+        getline(stream, temp, ',');
+        song.mode = stoi(temp);
+        getline(stream, temp, ',');
+        song.speechiness = stod(temp);
+        getline(stream, temp, ',');
+        song.tempo = stod(temp);
+        getline(stream, song.time_signature, ',');
+        getline(stream, temp, ',');
+        song.valence = stod(temp);
+        getline(stream, temp, '\n');
+        songList.push_back(song);
+    }
+
+    cout << songList.size() << endl;
     return 0;
 }
